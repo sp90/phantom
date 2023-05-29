7f3f4ffdff429d6810336f7a170abcdd80bd76de
robertIsaac <dev.robert.isaac@gmail.com>
2023-05-29 23:19:44 +0300

    update to latest packages
    remove unused interceptors

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,17 +1,10 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { ServerStateInterceptor } from './interceptors/server-transfer.interceptor';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ServerStateInterceptor,
-      multi: true
-    }
   ]
 };
 
